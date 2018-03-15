387450f77b01faa7118786daaf3cc1ae68fec038
minhkhang1795 <13646394+minhkhang1795@users.noreply.github.com>
2018-03-14 23:18:16 -0400

    Finishtower assignment

--- trees/day03/src/divide_and_conquer/RadioTowers.java
+++ trees/day03/src/divide_and_conquer/RadioTowers.java
@@ -7,6 +7,7 @@ public class RadioTowers {
     public static class Tower {
         public double x;
         public double y;
+
         public Tower(double x, double y) {
             this.x = x;
             this.y = y;
@@ -27,13 +28,37 @@ public class RadioTowers {
     // Return true if no two towers are within 1 mile
     public static boolean checkStrip(List<Tower> strip) {
         // TODO
-        return false;
+        for (int i = 0; i < strip.size(); i++) {
+            for (int j = i + 1; j < i + 8; j++) {
+                if (j < strip.size() && isWithin(strip.get(i), strip.get(j), 1)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     // Return true if no two towers are within distance 1 of each other
     public static boolean validTowers(List<Tower> Lx, List<Tower> Ly) {
         assert Lx.size() == Ly.size();
         // TODO
-        return false;
+        if (Ly.get(Ly.size() - 1).y - Ly.get(0).y <= 2) {
+            return checkStrip(Lx);
+        }
+
+        List<Tower> topLy = Ly.subList(0, Ly.size() / 2);
+        List<Tower> botLy = Ly.subList(Ly.size() / 2, Ly.size());
+        List<Tower> topLx = getLxFromLy(Lx, topLy.get(0).y, topLy.get(topLy.size()-1).y);
+        List<Tower> botLx = getLxFromLy(Lx, botLy.get(0).y, botLy.get(botLy.size()-1).y);
+        return validTowers(topLx, topLy) && validTowers(botLx, botLy);
+    }
+
+    private static List<Tower> getLxFromLy(List<Tower> Lx, double ymin, double ymax) {
+        List<Tower> result = new ArrayList<>();
+        for (Tower t : Lx) {
+            if (t.y <= ymax && t.y >= ymin)
+                result.add(t);
+        }
+        return result;
     }
 }
