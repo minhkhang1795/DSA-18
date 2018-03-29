600140f1470e325dddcde83b2f5bf1b60658a988
minhkhang1795 <13646394+minhkhang1795@users.noreply.github.com>
2018-03-29 00:42:32 -0400

    Finish 2 problems

--- graphs/day01/src/CoinsOnAClock.java
+++ graphs/day01/src/CoinsOnAClock.java
@@ -6,6 +6,31 @@ public class CoinsOnAClock {
     public static List<char[]> coinsOnAClock(int pennies, int nickels, int dimes, int hoursInDay) {
         // TODO
         List<char[]> result = new ArrayList<>();
+        boolean[] used = new boolean[hoursInDay];
+        coinsOnAClock(new char[hoursInDay], 0, used, pennies, nickels, dimes, hoursInDay, result);
         return result;
     }
+
+    private static void coinsOnAClock(char[] solution, int position, boolean[] used, int pennies, int nickels, int dimes, int hoursInDay, List<char[]> result) {
+        if (pennies + nickels + dimes == 0) {
+            result.add(solution);
+            return;
+        }
+        boolean[] newUsed = used.clone();
+        newUsed[position] = true;
+        if (pennies != 0 && !used[position]) {
+            solution[position] = 'p';
+            coinsOnAClock(solution.clone(), (position + 1) % hoursInDay, newUsed, pennies - 1, nickels, dimes, hoursInDay, result);
+        }
+        if (nickels != 0 && !used[position]) {
+            solution[position] = 'n';
+            coinsOnAClock(solution.clone(), (position + 5) % hoursInDay, newUsed, pennies, nickels - 1, dimes, hoursInDay, result);
+        }
+        if (dimes != 0 && !used[position]) {
+            solution[position] = 'd';
+            coinsOnAClock(solution.clone(), (position + 10) % hoursInDay, newUsed, pennies, nickels, dimes - 1, hoursInDay, result);
+        }
+    }
 }
+
+
--- graphs/day01/src/NQueens.java
+++ graphs/day01/src/NQueens.java
@@ -8,15 +8,15 @@ public class NQueens {
      * Checks the 45° and 135° diagonals for an existing queen. For example, if the board is a 5x5
      * and you call checkDiagonal(board, 3, 1), The positions checked for an existing queen are
      * marked below with an `x`. The location (3, 1) is marked with an `o`.
-     *
+     * <p>
      * ....x
      * ...x.
      * x.x..
      * .o...
      * .....
-     *
+     * <p>
      * Returns true if a Queen is found.
-     *
+     * <p>
      * Do not modify this function (the tests use it)
      */
     public static boolean checkDiagonal(char[][] board, int r, int c) {
@@ -52,7 +52,35 @@ public class NQueens {
     public static List<char[][]> nQueensSolutions(int n) {
         // TODO
         List<char[][]> answers = new ArrayList<>();
+        nQueensSolutions(new char[n][n], 0, answers);
         return answers;
+    }
+
+    private static void nQueensSolutions(char[][] current, int row, List<char[][]> answers) {
+        if (row == current.length) {
+            answers.add(current);
+            return;
+        }
+        for (int col = 0; col < current.length; col++) {
+            fillBoard(current, row, col);
+            if (!checkValid(current, row, col))
+                continue;
+            nQueensSolutions(copyOf(current), row + 1, answers);
+        }
+    }
+
+    private static boolean checkValid(char[][] current, int row, int col) {
+        for (int i = 0; i < row; i++) {
+            if (current[i][col] == 'Q')
+                return false;
+        }
+        return !checkDiagonal(current, row, col);
+    }
+
+    private static void fillBoard(char[][] current, int row, int col) {
+        for (int i = 0; i < current.length; i++) {
+            current[row][i] = i == col ? 'Q' : '.';
+        }
     }
 
 }
