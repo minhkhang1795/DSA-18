829eab2305448d1da965b9e8d4f6b2e889e636fc
minhkhang1795 <13646394+minhkhang1795@users.noreply.github.com>
2018-04-21 19:37:49 -0400

    LIS

--- DP/day00/src/DiceRollSum.java
+++ DP/day00/src/DiceRollSum.java
@@ -1,10 +1,26 @@
+import java.util.HashMap;
+
 public class DiceRollSum {
 
-    // Runtime: TODO
-    // Space: TODO
+    // Runtime: N
+    // Space: N
     public static int diceRollSum(int N) {
         // TODO
-        return 0;
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        return diceRollSumDP(N, memo);
+    }
+
+    private static int diceRollSumDP(int n, HashMap<Integer, Integer> memo) {
+        if (n < 0)
+            return 0;
+        if (n == 0)
+            return 1;
+        if (memo.containsKey(n))
+            return memo.get(n);
+        int r = diceRollSumDP(n - 1, memo) + diceRollSumDP(n - 2, memo) + diceRollSumDP(n - 3, memo) +
+                diceRollSumDP(n - 4, memo) + diceRollSumDP(n - 5, memo) + diceRollSumDP(n - 6, memo);
+        memo.put(n, r);
+        return r;
     }
 
 }
--- DP/day00/src/LongestIncreasingSubsequence.java
+++ DP/day00/src/LongestIncreasingSubsequence.java
@@ -1,9 +1,25 @@
+import jdk.nashorn.api.tree.BinaryTree;
+import jdk.nashorn.api.tree.ExpressionTree;
+import jdk.nashorn.api.tree.TreeVisitor;
+
+import java.util.ArrayList;
+
 public class LongestIncreasingSubsequence {
 
-    // Runtime: TODO
-    // Space: TODO
+    // Runtime: N^2
+    // Space: N
     public static int LIS(int[] A) {
         // TODO
-        return 0;
+        int max = -1;
+        int[] memo = new int[A.length];
+        for (int i = A.length - 1; i >= 0; i--) {
+            for (int j = i + 1; j < A.length; j++) {
+                if (A[i] < A[j]) {
+                    memo[i] = memo[i] < memo[j] + 1 ? memo[j] + 1 : memo[i];
+                }
+            }
+            max = memo[i] > max ? memo[i] : max;
+        }
+        return max + 1;
     }
 }
