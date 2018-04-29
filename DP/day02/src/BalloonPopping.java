5d35a0e1290ff38b1eb5ec531a96f23d52c12925
minhkhang1795 <13646394+minhkhang1795@users.noreply.github.com>
2018-04-29 19:28:59 -0400

    Balloon popping

--- DP/day02/src/BalloonPopping.java
+++ DP/day02/src/BalloonPopping.java
@@ -4,6 +4,26 @@ public class BalloonPopping {
 
     public static int maxPoints(int[] B) {
         // TODO
-        return 0;
+        int[][] pts = new int[B.length][B.length];
+        for (int len = 0; len < B.length; len++) {
+            for (int i = 0; i < B.length - len; i++) {
+                int j = i + len;
+                int max_pts = -1;
+
+                for (int k = i; k <= j; k++) {
+                    int B_left = i - 1 < 0 ? 1 : B[i - 1];
+                    int B_right = j + 1 >= B.length ? 1 : B[j + 1];
+                    int left_pts = i > k - 1 ? 0 : pts[i][k - 1];
+                    int right_pts = k + 1 > j ? 0 : pts[k + 1][j];
+                    int pts_temp = B_left * B[k] * B_right + left_pts + right_pts;
+                    if (pts_temp > max_pts) {
+                        max_pts = pts_temp;
+                    }
+                }
+                pts[i][j] = max_pts;
+            }
+        }
+        return pts[0][B.length - 1];
     }
+
 }
--- DP/day02/src/TextJustification.java
+++ DP/day02/src/TextJustification.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -6,17 +7,35 @@ public class TextJustification {
     private static double cost(String[] words, int lo, int hi, int m) {
         if (hi <= lo)
             throw new IllegalArgumentException("Hi must be higher than Lo");
-        int length = hi-lo-1; // account for spaces;
+        int length = hi - lo - 1; // account for spaces;
         for (int i = lo; i < hi; i++) {
             length += words[i].length();
         }
         if (length > m)
             return Double.POSITIVE_INFINITY;
-        return Math.pow(m-length, 3);
+        return Math.pow(m - length, 3);
     }
 
-    public static List<Integer> justifyText(String[] w, int m) {
-        return null;
+    static List<Integer> justifyText(String[] w, int m) {
+        int[] lineBreaks = new int[w.length + 1];
+        double[] dp = new double[w.length + 1];
+        dp[w.length] = 0;
+        for (int i = w.length - 1; i >= 0; i--) {
+            dp[i] = Double.MAX_VALUE;
+            for (int j = i + 1; j <= w.length; j++) {
+                if (dp[i] > dp[j] + cost(w, i, j, m)) {
+                    dp[i] = dp[j] + cost(w, i, j, m);
+                    lineBreaks[i] = j;
+                }
+            }
+        }
+        int i = 0;
+        List<Integer> result = new ArrayList<>();
+        while (i < w.length) {
+            result.add(i);
+            i = lineBreaks[i];
+        }
+        return result;
     }
 
 }
